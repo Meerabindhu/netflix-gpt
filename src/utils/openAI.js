8c6fe288cc76c82c35c5b70a4e21ef60250a4c71
Meera B <meera.b@tryzens.com>
2024-12-18 16:07:54 +0530

    REmoved error

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -1,5 +1,5 @@
 import OpenAI from 'openai';
-import { OPENAI_API_KEY } from './constants';
+// import { OPENAI_API_KEY } from './constants';
 
 const client = new OpenAI({
 //   apiKey: OPENAI_API_KEY, // This is the default and can be omitted
