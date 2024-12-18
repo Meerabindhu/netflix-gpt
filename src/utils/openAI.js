44efe2b33629d09bf1223b9b9b51854cf2346e53
Meera B <meera.b@tryzens.com>
2024-12-18 16:07:07 +0530

    Implemented GPT feature

--- src/utils/openAI.js
+++ src/utils/openAI.js
@@ -2,8 +2,8 @@ import OpenAI from 'openai';
 import { OPENAI_API_KEY } from './constants';
 
 const client = new OpenAI({
-  apiKey: OPENAI_API_KEY, // This is the default and can be omitted
-  dangerouslyAllowBrowser: true
+//   apiKey: OPENAI_API_KEY, // This is the default and can be omitted
+//   dangerouslyAllowBrowser: true
 });
 
 export default client
